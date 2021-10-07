5ec3d786c2d63e0ced15921d7d7a58ebb65c82dc
korzhenevskaya <kristinkor@gmail.com>
2021-10-07 16:12:13 -0500

    Refactor WinnerStatus for tests

--- src/main/java/edu/cuny/citytech/connect4/model/WinnerStatus.java
+++ src/main/java/edu/cuny/citytech/connect4/model/WinnerStatus.java
@@ -1,26 +1,37 @@
 package edu.cuny.citytech.connect4.model;
 
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class WinnerStatus {
-    private int[][] position;
-    private char whoWon;
+
+    private final int[][] position;
+    private final char whoWon;
 
     public WinnerStatus(int[][] position, char whoWon) {
         this.position = position;
         this.whoWon = whoWon;
     }
 
-    public Map<String, Object> toMap(){
-        Map<String,Object> map = new HashMap<>();
-        map.put("isWinner",this.isWinner());
-        map.put("position", this.position);
+    public Map<String, Object> toMap() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("isWinner", this.isWinner());
+        map.put("position", getSingleIndices());
         map.put("whoWon", this.whoWon);
         return map;
     }
 
     boolean isWinner() {
         return whoWon == 'X' || whoWon == 'O';
+    }
+
+    int[] getSingleIndices() {
+        return Arrays
+                .stream(position)
+                //pair is i and j
+                .map(pair -> pair[0] * 7 + pair[1])
+                .mapToInt(Integer::intValue)
+                .toArray();
     }
 }
